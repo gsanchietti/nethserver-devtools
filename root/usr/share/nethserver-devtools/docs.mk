578a72fb69f7b4ec200d8945b8ad91ec3d678814
Davide Principi <davide.principi@nethesis.it>
2014-01-27 14:52:51 +0100

    Grab rst2html parameters from a first line comment. Refs #2627

--- root/usr/share/nethserver-devtools/docs.mk
+++ root/usr/share/nethserver-devtools/docs.mk
@@ -4,12 +4,16 @@
 
 RSTFILES        := $(shell find . -name '*.rst')
 HTMLFILES       := $(addsuffix .html, $(basename ${RSTFILES}))
-RST2HTML        := rst2html 
+RST2HTML        := rst2html --link-stylesheet --stylesheet-path= --stylesheet= \
+                            --no-doc-title --no-doc-info
 
-.PHONY: all
+.PHONY: all clean
 
 all: ${HTMLFILES}
 
 %.html: %.rst
-	${RST2HTML} $< $@
+	args=`head -1 $< | sed -r -n '/^\.\. +/ {s/^\.\. +//; p}'` ; \
+	${RST2HTML} $${args} $< > $@ ; \
 
+clean:
+	rm -f ${HTMLFILES}
